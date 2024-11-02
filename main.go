f01bcd3923598f0f9c6faac17c5e83d7ea0c58f5
André Freitas <freitasandre38@gmail.com>
2024-11-02 08:45:27 -0300

    feat: create http duration with histrogram

--- main.go
+++ main.go
@@ -22,18 +22,30 @@ var httpRequests = prometheus.NewCounterVec(prometheus.CounterOpts{
 	Help: "Count the amount of all HTTP request for goapp",
 }, []string{})
 
+var httpDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+	Name: "goapp_http_request_duration",
+	Help: "Represents duration in seconds of all http requests",
+}, []string{"handler"})
+
 func main() {
 	fmt.Println("Starting Gauge metric")
 	r := prometheus.NewRegistry()
 	r.MustRegister(onlinesUsers)
 	r.MustRegister(httpRequests)
+	r.MustRegister(httpDuration)
 
 	home := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Handle"))
 	})
+
+	d := promhttp.InstrumentHandlerDuration(
+		httpDuration.MustCurryWith(prometheus.Labels{"handler": "home"}),
+		promhttp.InstrumentHandlerCounter(httpRequests, home),
+	)
+
 	http.Handle("/metrics", promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
-	http.Handle("/metrics/counter", promhttp.InstrumentHandlerCounter(httpRequests, home))
+	http.Handle("/metrics/counter", d)
 
 	log.Fatal(http.ListenAndServe(":8181", nil))
 }
