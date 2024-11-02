644c9dc5a67937320c6a7973b6f54c0c98391067
André Freitas <freitasandre38@gmail.com>
2024-11-02 08:40:00 -0300

    feat: create handle func home to deal with instrument handler counter

--- main.go
+++ main.go
@@ -17,10 +17,23 @@ var onlinesUsers = prometheus.NewGauge(prometheus.GaugeOpts{
 	},
 })
 
+var httpRequests = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Name: "goapp_http_requests_total",
+	Help: "Count the amount of all HTTP request for goapp",
+}, []string{})
+
 func main() {
 	fmt.Println("Starting Gauge metric")
 	r := prometheus.NewRegistry()
 	r.MustRegister(onlinesUsers)
+	r.MustRegister(httpRequests)
+
+	home := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Handle"))
+	})
 	http.Handle("/metrics", promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
+	http.Handle("/metrics/counter", promhttp.InstrumentHandlerCounter(httpRequests, home))
+
 	log.Fatal(http.ListenAndServe(":8181", nil))
 }
